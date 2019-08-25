4d18f0dd914099fe0eacdb75aebbd5b3450bc222
Jurian Baas <jurian.baas@gmail.com>
2019-08-25 16:11:36 +0200

    Sketching out global feedback window and annotations

--- src/App.svelte
+++ src/App.svelte
@@ -9,31 +9,44 @@
   import canvg from "canvg";
 
   // Settings
-  const activeBars = ["Week 1", ""];
-  const padding = { top: 250, right: 0, bottom: 120, left: 120 };
-  let width = 800;
-  let height = 800;
+  const padding = { top: 200, right: 0, bottom: 120, left: 120 };
+  let width = 600;
+  let height = 600;
 
   // Helpers
   const activeBar = name => activeBars.includes(name);
   const int = format(",.0f");
-  const params = new URLSearchParams(window.location.search);
 
   // Default data, not used yet
-  export let dataDefault = [
-    { name: "Vorig jaar", value: 20 },
-    { name: "Week 1", value: 20 },
-    { name: "Week 2", value: 20 },
-    { name: "Week 3", value: 20 },
-    { name: "Week 4", value: 20 }
+  // export let dataDefault = [
+  //   { name: "Weekverbruik vorig jaar", value: 20 },
+  //   { name: "Week 1", value: 20 },
+  //   { name: "Week 2", value: 20 },
+  //   { name: "Week 3", value: 20 },
+  //   { name: "Week 4", value: 20 }
+  // ];
+
+  const barNames = [
+    "Weekverbruik vorig jaar",
+    "Week 1",
+    "Week 2",
+    "Week 3",
+    "Week 4"
   ];
 
-  const barNames = ["Vorig jaar", "Week 1", "Week 2", "Week 3", "Week 4"];
-
+  // URL paramater parsing and settings
+  const params = new URLSearchParams(window.location.search);
   const urlData = params
     .get("data")
     .split(",")
     .map(Number);
+
+  const urlWeeks =
+    params.get("weeks") != null ? params.get("weeks").split(",") : "";
+
+  const activeBars = urlWeeks;
+
+  const urlWeek = urlData.length - 1;
 
   $: dataAll = urlData.map((el, i) => ({
     name: barNames[i],
@@ -59,7 +72,7 @@
   $: x = scaleBand()
     .domain(data.map(d => d.name))
     .range([padding.left + 60, width - padding.right]) // Offset of 60 pixels, manual first bar with more padding
-    .padding(0.3);
+    .padding(0.25);
 
   $: y = scaleLinear()
     .domain([0, dataMax])
@@ -68,34 +81,14 @@
 
   $: yTicks = y.ticks(5);
   $: barWidth = x.bandwidth();
-
-  onMount(() => {
-    var svg = document.querySelector("svg");
-    var canvas = document.createElement("canvas");
-    canvas.height = height;
-    canvas.width = width;
-    canvg(canvas, svg.parentNode.innerHTML.trim());
-    console.log(svg.parentNode.innerHTML.trim());
-    console.log(canvg(canvas, svg.parentNode.innerHTML.trim()));
-    // var dataURL = canvas.toDataURL("image/png");
-    // var data = atob(dataURL.substring("data:image/png;base64,".length)),
-    //   asArray = new Uint8Array(data.length);
-
-    // for (var i = 0, len = data.length; i < len; ++i) {
-    //   asArray[i] = data.charCodeAt(i);
-    // }
-
-    // var blob = new Blob([asArray.buffer], { type: "image/png" });
-    // saveAs(blob, "export_" + Date.now() + ".png");
-  });
 </script>
 
 <style>
   svg {
     position: relative;
-    width: 800px;
-    height: 800px;
-    border: 1px solid black;
+    width: 600px;
+    height: 600px;
+    border: 1px solid lightgrey;
   }
 </style>
 
@@ -104,48 +97,72 @@
 
     <!-- annotations -->
     <g class="annotations-manual">
+
+      <!-- arrow for testweek -->
       <path
-        d="M {padding.left - 25}
-        {y(dataAll[0].value) - 5} C 100 50 {padding.left - 20}
-        {padding.left - 20}
-        {width / 2 - 100}
-        {padding.top - 80}"
-        style="stroke:grey;stroke-width:1;fill:transparent" />
-
-      <g transform="translate({width / 2 - 100},{padding.top - 130})">
-        <rect
-          class="annotation-note-bg"
-          width="200"
-          height="100"
-          x="0"
-          y="0"
-          fill="goldenrod"
-          fill-opacity="0.2" />
+        d="M 100 {y(dataAll[0].value) - 5} C 40 100 100 110 100 110"
+        class="arrow" />
+      <g transform="translate(100,50)">
+        <path d="M -9 67 c 2-3 6-4 9-7-3-1-9-3-11-6" class="arrow" />
       </g>
+    </g>
 
-      <g
-        class="annotation-note-content"
-        transform="translate({width / 2}, {padding.top - 90})">
-        <text class="annotation-note-label" dx="0" y="30" fill="grey">
-          <tspan x="0" dy="0">Week 1</tspan>
-        </text>
-        <text class="annotation-note-title" fill="grey" font-weight="bold">
-          <tspan x="0" dy="0">⚡ {int(dataTestweek.value)} kWh ⚡</tspan>
-        </text>
-      </g>
+    <!-- arrow for week 1 -->
+    <path
+      d="M {x(data[0].name) + barWidth / 2}
+      {y(data[0].value) - 5} C 270 200 270 270 290 170"
+      class="arrow" />
+    <g transform="translate(230,170) rotate(-80 -5 -5)">
+      <path class="arrow" d="M -9 67 c 2-3 6-4 9-7-3-1-9-3-11-6" />
+    </g>
+
+    <rect
+      class="annotation-note-bg"
+      width="400"
+      height="110"
+      x="100"
+      y="60"
+      fill="goldenrod"
+      fill-opacity="0.2" />
+
+    <g class="annotation-note-content" transform="translate({width / 2}, 86)">
+      <text class="annotation-note-label">
+        <tspan x="0" dy="0" style="font-size:3em">⚡</tspan>
+      </text>
+      <text class="annotation-note-label">
+        <tspan x="0" dy="-20" style="font-size:0.7em">❌</tspan>
+      </text>
+      <text class="annotation-note-label">
+        <tspan x="0" dy="-20" style="font-size:0.7em">✅</tspan>
+      </text>
+      <text class="annotation-note-label" dx="0" y="30">
+        <tspan x="0" dy="-5">Mooi op schema. Ga zo door!</tspan>
+      </text>
+      <text
+        class="annotation-note-label"
+        dx="0"
+        y="30"
+        style="font-size:.9em; font-family: 'AvenirNextLTW01-Regular'">
+        <tspan x="0" dy="20">
+          Jullie zitten nu 7% onder het jullie gebruik van vorig jaar.
+        </tspan>
+      </text>
+      <!-- <text class="annotation-note-title">
+        <tspan x="0" dy="0">⚡ {int(dataTestweek.value)} kWh ⚡</tspan>
+      </text> -->
     </g>
 
     <!-- bars -->
     <g class="bars">
       <rect
-        x={padding.left - barWidth + 21}
+        x={padding.left - barWidth + 25}
         y={y(dataTestweek.value)}
         width={barWidth - 4}
         height={height - padding.bottom - y(dataTestweek.value)}
         class="testweek" />
       <text
         class="testweek"
-        x={padding.left - 27}
+        x={padding.left - 14}
         y={y(dataTestweek.value) + 35}>
         {int(dataTestweek.value)} kWh
       </text>
@@ -156,10 +173,10 @@
           y={y(dataPoint.value)}
           width={barWidth - 4}
           height={height - padding.bottom - y(dataPoint.value)}
-          class={activeBar(dataPoint.name) ? 'active' : 'inactive'} />
+          class={activeBar(dataPoint.name) ? 'known' : 'unknown'} />
         <text
-          class={activeBar(dataPoint.name) ? 'active' : 'inactive'}
-          x={x(dataPoint.name) + x.bandwidth() / 2 - 3}
+          class={activeBar(dataPoint.name) ? 'known' : 'unknown'}
+          x={x(dataPoint.name) + barWidth / 2 - 3}
           y={y(dataPoint.value) + 35}>
           {activeBar(dataPoint.name) ? `${int(dataPoint.value)} kWh` : '?'}
         </text>
@@ -178,7 +195,7 @@
     <!-- x axis bar labels -->
     <g class="axis x-axis">
       <g class="tick" transform="translate(15,{height})">
-        <text x={padding.left - 42} y={-padding.bottom + 35}>
+        <text x={padding.left - 30} y={-padding.bottom + 35}>
           {dataTestweek.name}
         </text>
       </g>
@@ -192,9 +209,4 @@
     </g>
   </svg>
 </div>
-<!-- <button on:click={() => tweenedData.set(data2)}>data2</button> -->
-<!-- {activeBar('1990')} -->
-<!-- {annotationGen()} -->
-<!-- {dataTestweek} {y(dataTestweek)} {barWidth} -->
-<!-- {dataMax} {dataMax} {linegen(dataIncoming)} -->
-<!-- {width} -->
+{urlWeek}
